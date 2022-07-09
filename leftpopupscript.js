502fb2666d816f6d1913047230d1f1057dc9db77
ABHAY-9565KUMAR <108982024+ABHAY-9565KUMAR@users.noreply.github.com>
2022-07-09 17:17:29 +0530

    Add files via upload

---
+++ leftpopupscript.js
@@ -0,0 +1,18 @@
+ 
+
+ function toggle(){
+    var blur=document.getElementById('blur');
+    blur.classList.toggle('active');    
+       var popup=document.getElementById('popup');
+        popup.classList.toggle('active');}
+
+
+
+        function togggle(){
+    var blur=document.getElementById('blur');
+    blur.classList.toggle('active');    
+       var popupp=document.getElementById('popupp');
+        popupp.classList.toggle('active');}
+
+       
+           
